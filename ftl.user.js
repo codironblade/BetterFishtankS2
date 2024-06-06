e657ee2e83a304c039492b65803d9892e234c76d
codironblade <codironblade@gmail.com>
2024-06-06 18:39:07 -0500

    Update ftl.user.js
    rewrite for 2.5 layout
    new background setting
    hide popups

--- ftl.user.js
+++ ftl.user.js
@@ -4,7 +4,7 @@
 // @match       *://*.fishtank.live/*
 // @grant       GM_getValue
 // @grant       GM_setValue
-// @version     0.93
+// @version     0.94
 // @author      codironblade
 // @homepageURL https://github.com/codironblade/BetterFishtankS2
 // @updateURL    https://raw.githubusercontent.com/codironblade/BetterFishtankS2/main/ftl.user.js
@@ -19,11 +19,9 @@ const clickThing = function(v) {
     v.click();
     document.activeElement?.blur();
 }
-const roomMapArray = [[5,2],[3,6],[3,2],[3,3],[3,5],[5,5],[5,6],[5,7],[6,3],[4,6],[4,5]];
-const roomMapDict = {["bedroom 1"]:[5,2],["bedroom 2"]:[3,6],["bedroom 3"]:[3,2],["bunk"]:[3,3],["hallway upstairs"]:[3,5],["hallway downstairs"]:[5,5],["living room"]:[5,6],["lounge"]:[5,7],["bar"]:[6,3],["kitchen"]:[4,6],["dog house"]:[4,5]};
-const settings = {CHtts:false,CHsfx:false,CHemote:false,CHhappening:false,CHsystem:false,CHclan:false,CHdefault:false,ttstime:45,bg:"Blue",muteAll:false};
+const settings = {CHtts:false,CHsfx:false,CHemote:false,CHhappening:false,CHsystem:false,CHclan:false,CHdefault:false,bgbr:40,bg:"Default",muteAll:false};
 const settingsInfo = [["CHtts","Chat hide TTS"],["CHsfx","Chat hide SFX"],["CHemote","Chat hide emotes/commands"],["CHhappening","Chat hide items"],["CHsystem","Chat hide system"],["CHclan","Chat hide clan stuff"],
-                      ["CHdefault","Chat hide chats"],["ttstime","TTS popup seconds"],["bg","Background Image","Blue","Dark","Default"],["muteAll","Mute all UI sounds"]];
+                      ["CHdefault","Chat hide chats"],["bgbr","Background Brightness"],["bg","Background Image","Blue","Dark","S2 Green","Default"],["muteAll","Mute all UI sounds"]];
 const savedStr = GM_getValue("ftlsave");
 //console.log("savedStr:",savedStr);
 for (const [k,v] of Object.entries( savedStr ? JSON.parse(savedStr) : {} )) {
@@ -46,13 +44,19 @@ const editStyle = function(v,editsList){
         });
     }
 }
+let defaultbg = "";
 document.arrive(".background_background__fNMDL",{onceOnly:true,existing:true},function(v){
+    defaultbg = v.style.backgroundImage;
     if (settings.bg === "Blue") {
         v.style.backgroundImage = "url(https://i.imgur.com/mbrRNrf.jpeg)";
     } else if (settings.bg === "Dark") {
         v.style.backgroundImage = "none";
         v.style.backgroundColor = "#171616";
+    } else if (settings.bg === "S2 Green") {
+        v.style.backgroundImage = "url(https://cdn.fishtank.live/images/patterns/green-bg.png)";
+        v.style.backgroundSize = "cover";
     }
+    v.style.opacity = (settings.bgbr > 99) ? 1 : "0."+settings.bgbr;
     //remove scanlines
     let removeFilter = document.createElement('style');
     removeFilter.textContent = 'body::after { content: none }';
@@ -60,42 +64,15 @@ document.arrive(".background_background__fNMDL",{onceOnly:true,existing:true},fu
 });
 document.arrive("main",{onceOnly:true,existing:true},function(v){
     v.style.gridTemplateRows = "52px auto 1fr auto";
-    v.style.gridTemplateColumns = "220px auto 360px 360px";
+    v.style.gridTemplateColumns = "220px auto 360px";
 });
 
 document.arrive("#main-panel",{onceOnly:true},function(m){
-    m.style.gridRow = "3/4";
+    //m.style.gridRow = "3/4";
     m.arrive("#live-stream-player",async function(v){
         //handle new player
         v.parentElement.style.zIndex = 99;
-        editStyle(v,playerEdits);
-        let playing = Date.now();
-        while (document.contains(v)) {
-            await sleep(0.2);
-            const player = v.querySelector("video");
-            if (!player) {
-                return;
-            }
-            if (player.paused===false || player.readyState > 0) {
-                playing=Date.now();
-            } else if ((Date.now()-playing)/1000 > 3.5) {
-                //unfreeze the stream
-                const curcam = document.querySelector(".live-stream-fullscreen_name__C3TdW").textContent;
-                const autocams = document.querySelector(".live-streams-auto-mode_live-streams-auto-mode__pE2X_ > .checkbox_checked__ibaIs");
-                autocams?.click();
-                await sleep(0.05);
-                document.querySelector(".live-stream-fullscreen_close__JY_lb > button")?.click();
-                await sleep(0.05);
-                document.getElementById(curcam.toLowerCase().replace(" ","-"))?.click();
-                await sleep(0.1);
-                autocams?.click();
-                return;
-            }
-            const slider = document.querySelector(".live-stream-volume_slider__s0Oqh");
-            if (slider) {
-                player.volume = slider.valueAsNumber/100;
-            }
-        }
+        //editStyle(v,playerEdits);
     });
     m.arrive(".happening_item__Y7BtW",async function(v){
         //move the big fishtoy popup
@@ -109,21 +86,10 @@ document.arrive("#main-panel",{onceOnly:true},function(m){
         clone.remove();
     });
 });
-document.arrive(".house-map-panel_house-map-panel__yJhI_",function(v){
-    editStyle(v,mapEdits);
-});
-document.arrive(".stats_stats__SIg_t",function(v){
-    if (v.classList.length === 1) {
-        v.style.alignSelf = "start";
-    }
-});
-document.arrive(".secondary-panel_secondary-panel__vUc65",{onceOnly:true},function(v){
-    document.arrive(".experience-bar_experience-bar__nVDge",{onceOnly:true,existing:true},function(e){
+document.arrive(".home_left__UiQ0z",{onceOnly:true},function(v){
+    document.arrive(".status-bar_xp__VzguC",{onceOnly:true,existing:true},function(e){
         v.prepend(e);
     });
-});
-document.arrive(".secondary-panel_header__MbhpO",{onceOnly:true},function(v){
-    v.style.width = "23%";
 });
 //hide season pass
 document.arrive(".toast_season-pass__cmkhU",function(v){
@@ -154,19 +120,6 @@ document.arrive(".toast_message__l35K3 > h3",function(v){
 })
 
 //chat stuff
-let ttsid = 0;
-let ttsdate = 0;
-const ttsEdit = function(disp){
-    document.querySelector(".tts-history_tts-history__8_9eB").style.display = disp;
-}
-const ttsmu = (new MutationObserver(function(){
-    ttsEdit("flex");
-    if ((Date.now()-ttsdate)/1000 > 0.2 && document.querySelector('.feature-toggle-status_enabled__yN33i > img[alt^="FISHTOYS"]')) {
-        document.querySelector(".tts-history_text__ZVdV8").style.color="cyan";
-    }
-    window.clearTimeout(ttsid);
-    ttsid = window.setTimeout(ttsEdit,settings.ttstime*1000,"none");
-}));
 document.arrive("#chat-messages > div",async function(v){
     v.parentElement.parentElement.style.padding = "0px";
     for (const[set,value] of Object.entries(settings)) {
@@ -175,79 +128,14 @@ document.arrive("#chat-messages > div",async function(v){
                 v.style.display = "none";
                 //v.style.backgroundColor = "red";
             }
-            if (set !== "CHtts") {
-                return;
-            }
-            //update the "last tts"
-            await sleep(0.5);
-            let text = v.querySelector(".chat-message-tts_message__sWVCc").textContent;
-            const textElement = document.querySelector(".tts-history_text__ZVdV8");
-            const roomElement = document.querySelector(".tts-history_room__QNUZ0");
-            const timeElement = document.querySelector(".tts-history_timestamp__mVYdp");
-            const roomText = v.querySelector(".chat-message-tts_room__1lmqo").textContent.toLowerCase();
-            if (!timeElement) {
-                return;
-            }
-            ttsdate = Date.now();
-            document.querySelector(".tts-history_title__sfog8").childNodes[0].textContent = "New TTS";
-            const userNodes = document.querySelector(".tts-history_user__Wzyf_").childNodes;
-            if (userNodes.length>1) {
-                userNodes[0].style.display = "none";
-            }
-            userNodes[userNodes.length-1].textContent = v.querySelector(".chat-message-tts_from__1QSqc").textContent;
-            textElement.style.color = "#ff1d00";
-            textElement.style.animationTimingFunction = "steps(600, start)";
-            textElement.style.textShadow = "0 0 8px #bd0000";
-            if (textElement.childNodes[1]) {
-                text += " | last toy";
-            }
-            textElement.childNodes[0].textContent = text;
-            ttsmu.observe(textElement.childNodes[0],{characterData:true})
-            if (roomElement) {
-                roomElement.textContent = roomText;
-            } else {
-                const re = document.createElement("div");
-                re.className="tts-history_room__QNUZ0";
-                re.textContent = roomText;
-                timeElement.parentElement.prepend(re);
-            }
-            timeElement.textContent = v.querySelector(".chat-message-tts_timestamp__pIVv0").textContent;
-            const roomPair = roomMapDict[roomText];
-            if (roomPair) {
-                //highlight tts on map
-                const btnContainer = document.querySelector(".house-map-panel_click-area__xswcw:nth-child("+(roomPair[0])+")");
-                const roomBtn = btnContainer?.querySelector("button:nth-child("+(roomPair[1])+")");
-                if (roomBtn) {
-                    for (let i=0; i<Math.min(5,settings.ttstime); i++) {
-                        const curcam = document.querySelector(".live-stream-fullscreen_name__C3TdW")?.textContent.toLowerCase();
-                        if (curcam !== roomText) {
-                            roomBtn.style.opacity = "50%";
-                            roomBtn.style.backgroundColor = "yellow";
-                        }
-                        await sleep(0.5);
-                        roomBtn.style.opacity = null;
-                        roomBtn.style.backgroundColor = null;
-                        await sleep(0.5);
-                    }
-                }
-            }
-            return;
         }
     }
 });
-document.arrive(".tts-history_text__ZVdV8",{onceOnly:true},async function(v){
-    //hide last tts at start
-    await sleep(3);
-    ttsmu.observe(v.childNodes[0],{characterData:true})
-    await sleep(15);
-    if (ttsid === 0) {
-        v.parentElement.parentElement.parentElement.style.display = "none";
-    }
-});
-//quieter fishtoy
+//edits sounds
 const oldPlay = HTMLAudioElement.prototype.play;
+let tempMute = false;
 HTMLAudioElement.prototype.play = function () {
-    if (settings.muteAll) {
+    if (settings.muteAll || tempMute || this.src.substring(33,39)==="popup-") {
         this.volume = 0;
     } else if (this.src.substring(33) === "fishtoy.wav") {
         this.volume = this.volume / 3;
@@ -325,6 +213,9 @@ const onSetBtnClick = function() {
                 if (settings[setKey] === n) return;
                 settings[setKey] = n;
                 save();
+                if (setKey === "bgbr") {
+                    document.querySelector(".background_background__fNMDL").style.opacity = (settings.bgbr > 99) ? 1 : "0."+settings.bgbr;
+                }
             }
             box.addEventListener("blur",textBoxChanged);
             box.addEventListener("keypress",textBoxChanged);
@@ -348,13 +239,17 @@ const onSetBtnClick = function() {
                 save();
                 if (setKey === "bg") {
                     const back = document.querySelector(".background_background__fNMDL");
+                    back.style.backgroundSize = null;
                     if (settings.bg === "Blue") {
                         back.style.backgroundImage = "url(https://i.imgur.com/mbrRNrf.jpeg)";
                     } else if (settings.bg === "Dark") {
                         back.style.backgroundImage = "none";
                         back.style.backgroundColor = "#171616";
-                    } else {
+                    } else if (settings.bg === "S2 Green") {
                         back.style.backgroundImage = "url(https://cdn.fishtank.live/images/patterns/green-bg.png)";
+                        back.style.backgroundSize = "cover";
+                    } else {
+                        back.style.backgroundImage = defaultbg;
                     }
                 }
             });
@@ -370,17 +265,6 @@ const onSetBtnClick = function() {
     }));
     mut.observe(m,{childList:true});
 }
-/*/ old ui
-document.arrive(".settings-modal_password___da3r  > button > div",function(tocloned){
-    if (document.contains(ourBtn)) {
-        return;
-    }
-    ourBtn = tocloned.parentElement.parentElement.cloneNode(true);
-    tocloned.parentElement.parentElement.parentElement.appendChild(ourBtn);
-    ourBtn.querySelector("button > div").textContent = "SCRIPT";
-    ourBtn.querySelector("button").addEventListener("click",onSetBtnClick);
-})
-/*/
 document.arrive(".settings-modal_password___da3r",function(tocloned){
     //new ui
     if (document.contains(ourBtn)) {
@@ -404,6 +288,13 @@ document.arrive(".clip-player_date__Xk3xl",{existing:true},function(v){
         window.open(document.querySelector('video').src);
     });
 });
+//hide popups
+document.arrive(".faction-troll_pop-up__i5p89",async function(v){
+    tempMute=true;
+    v.click();
+    await sleep(0.2);
+    tempMute=false;
+});
 //S2 ARCHIVE
 const archiveState = {r:'',d:'',h:'',v:null};
 const onLoadingArchive = async function(){
@@ -416,8 +307,7 @@ const onLoadingArchive = async function(){
         if (newState.d.length === 0) {
             archiveState.doseek = true;
             let anyRed = false;
-            const oldSet = settings.muteAll;
-            settings.muteAll = true;
+            tempMute = true;
             for (let i=1; i<3; i++) {
                 const stateKey = (i===1 ? "d" : "h")
                 const label = selectLabels[i];
@@ -433,7 +323,7 @@ const onLoadingArchive = async function(){
                     await sleep(0.2);
                 }
             }
-            settings.muteAll = oldSet;
+            tempMute=false;
         }
     } else {
         archiveState.doseek = false;
@@ -441,7 +331,7 @@ const onLoadingArchive = async function(){
     archiveState.r = newState.r; archiveState.d = newState.d; archiveState.h = newState.h;
 };
 document.arrive(".s2_body__Zco_w",{existing:true},function(s2body){
-    s2body.style.width = "1240px";
+    s2body.style.width = "1300px";
     archiveState.init = true;
     s2body.arrive(".s2_download__ji7Ga",function(v){
         v.style.position = "absolute";
